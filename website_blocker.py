5bbb9d660c956c862ac5d135b0ae9db9af8b30cf
Benyamin Abbasi <abbasi.benyamin@gmail.com>
2019-02-10 15:02:28 +0330

    just skipping the course

---
+++ website_blocker.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime as dt
+
+#we can use r:"C:\windows....
+host_path="C:\\Windows\\system32\\drivers\\etc\\hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com", "facebook.com"]
+
+while True:
+	print(1)
+	time.sleep(5)
+	
